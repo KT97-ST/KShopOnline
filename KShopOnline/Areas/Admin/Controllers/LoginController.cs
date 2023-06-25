53baa93e4e88ee25c030d3004e75ed657c01aa06
KOCoders <74465408+KOCoders@users.noreply.github.com>
2023-06-25 15:59:44 +0700

    show category list

--- KShopOnline/Areas/Admin/Controllers/LoginController.cs
+++ KShopOnline/Areas/Admin/Controllers/LoginController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Models;
+using System.Web.Security;
 namespace KShopOnline.Areas.Admin.Controllers
 {
     public class LoginController : Controller
@@ -21,10 +22,11 @@ namespace KShopOnline.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(LoginModel model)
         {
-            var res = new AccountModel().Login(model.Username, model.Password);
-            if(res == true &&ModelState.IsValid)
+            //var res = new AccountModel().Login(model.Username, model.Password);
+            if(Membership.ValidateUser(model.Username, model.Password) == true &&ModelState.IsValid)
             {
-                SessionHelper.SetSession(new UserSession() { Username = model.Username});
+                //SessionHelper.SetSession(new UserSession() { Username = model.Username});
+                FormsAuthentication.SetAuthCookie(model.Username, model.RememberMe);
                 return RedirectToAction("Index", "Home");
             }
             else
@@ -32,6 +34,12 @@ namespace KShopOnline.Areas.Admin.Controllers
                 ModelState.AddModelError("", "Ten dang nhap hoac mat khau khong dung");
             }
             return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
---
+++ Models/CategoryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Framework;
+using System.Data.SqlClient;
+
+namespace Models
+{
+    public class CategoryModel
+    {
+        private KShopDBContext context = null;
+        public CategoryModel()
+        {
+            context = new KShopDBContext();
+        }
+
+        public List<Category> ListAll()
+        {
+            var listCategory = context.Database.SqlQuery<Category>("Sp_Category_ListAll").ToList();
+            return listCategory;
+        }
+    }
+}
